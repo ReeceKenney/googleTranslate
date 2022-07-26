08121d41529ed32446b7f05790ef2b428ca03326
Courses <courses@MacBook-Pro.local>
2022-07-26 17:59:50 +0100

    customFonts

--- App.js
+++ App.js
@@ -8,6 +8,11 @@ import SettingsScreen from './screens/SettingsScreen';
 import SavedScreen from './screens/SavedScreen';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Entypo, Ionicons } from '@expo/vector-icons';
+import * as SplashScreen from 'expo-splash-screen';
+import { useCallback, useEffect, useState } from 'react';
+import * as Font from 'expo-font';
+
+SplashScreen.preventAutoHideAsync();
 
 const Tab = createBottomTabNavigator();
 
@@ -48,9 +53,52 @@ const TabNavigator = () => {
 const Stack = createStackNavigator();
 
 export default function App() {
+
+  const [appIsLoaded, setAppIsLoaded] = useState(false);
+
+  useEffect(() => {
+    const prepare = async () => {
+      try {
+        await Font.loadAsync({
+          black: require("./assets/fonts//Roboto-Black.ttf"),
+          blackItalic: require("./assets/fonts/Roboto-BlackItalic.ttf"),
+          bold: require("./assets/fonts/Roboto-Bold.ttf"),
+          boldItalic: require("./assets/fonts/Roboto-BoldItalic.ttf"),
+          italic: require("./assets/fonts/Roboto-Italic.ttf"),
+          light: require("./assets/fonts/Roboto-Light.ttf"),
+          lightItalic: require("./assets/fonts/Roboto-LightItalic.ttf"),
+          medium: require("./assets/fonts/Roboto-Medium.ttf"),
+          mediumItalic: require("./assets/fonts/Roboto-MediumItalic.ttf"),
+          regular: require("./assets/fonts/Roboto-Regular.ttf"),
+          thin: require("./assets/fonts/Roboto-Thin.ttf"),
+          thinItalic: require("./assets/fonts/Roboto-ThinItalic.ttf"),
+        });
+      }
+      catch (e) {
+        console.log(e);
+      }
+      finally {
+        setAppIsLoaded(true);
+      }
+    };
+
+    prepare();
+
+  }, []);
+
+  const onLayout = useCallback(async () => {
+    if (appIsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsLoaded]);
+  
+  if (!appIsLoaded) {
+    return null;
+  }
+
   return (
     <NavigationContainer>
-      <View style={{ flex: 1 }}>
+      <View onLayout={onLayout} style={{ flex: 1 }}>
         <Stack.Navigator>
           <Stack.Group>
             <Stack.Screen
